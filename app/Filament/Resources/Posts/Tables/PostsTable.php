9b3c703254a7c3ee788823b4a802beeaae03a901
Harutoo <herconaryangga15@gmail.com>
2026-04-22 11:12:09 +0700

    JS 9

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -5,12 +5,15 @@ namespace App\Filament\Resources\Posts\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 
 class PostsTable
 {
@@ -19,18 +22,65 @@ class PostsTable
         return $table
             ->columns([
             //
-            TextColumn::make('title'),
-            TextColumn::make('slug'),
-            TextColumn::make('category.name'),
-            ColorColumn::make('color'),
+            TextColumn::make('id')
+                ->label('ID')
+                ->sortable()
+                ->toggleable(),
+            TextColumn::make('title')
+                ->label('Title')
+                ->sortable()
+                ->searchable()
+                ->toggleable(),
+            TextColumn::make('slug')
+                ->label('Slug')
+                ->sortable()
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            TextColumn::make('category.name')
+                ->label('Category')
+                ->sortable()
+                ->searchable()
+                ->toggleable(),
+            ColorColumn::make('color')
+                ->label('Color')
+                ->toggleable(),
             ImageColumn::make('image')
-                ->disk('public'),
+                ->disk('public')
+                ->toggleable(),
+            TextColumn::make('tags')
+                ->label('Tags')
+                ->toggleable(isToggledHiddenByDefault: true),
             IconColumn::make('published')
-                ->label('Published Status'),
-            ])
+                ->label('Published Status')
+                ->boolean()
+                ->toggleable(),
+            TextColumn::make('created_at')
+                ->label('Created At')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(),
+            ])->defaultSort('created_at', 'desc')
+
             ->filters([
                 //
-            ])
+                Filter::make('created_at')
+                    ->label('Creation Date')
+                        ->schema([
+                            DatePicker::make('created_at')
+                                ->label('Select Date: '),
+                        ])
+                        ->query(function ($query, $data) {
+                            return $query
+                                ->when(
+                                    $data['created_at'],
+                                    fn ($query, $date) => $query->whereDate('created_at', $date)
+                                );
+                        }),
+                    SelectFilter::make('category_id')
+                        ->relationship('category', 'name')
+                        ->label('Category')
+                        ->preload(),
+                ])
             ->recordActions([
                 EditAction::make(),
             ])
